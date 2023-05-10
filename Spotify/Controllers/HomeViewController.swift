7e11092ed6c694ca4e811014118528ca01f058f5
Alex Ochigov <alex.ochigov@gmail.com>
2023-05-10 19:46:55 +0300

    setup tabbar

--- Spotify/Controllers/HomeViewController.swift
+++ Spotify/Controllers/HomeViewController.swift
@@ -12,8 +12,7 @@ class HomeViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
-        title = "Home"
-        view.backgroundColor = .white
+        view.backgroundColor = .systemBackground
     }
 }
 
--- Spotify/Controllers/TabBarViewController.swift
+++ Spotify/Controllers/TabBarViewController.swift
@@ -8,5 +8,33 @@
 import UIKit
 
 class TabBarViewController: UITabBarController {
-
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        let homeVC = HomeViewController()
+        let searchVC = SearchViewController()
+        let libraryVC = LibraryViewController()
+        
+        homeVC.title = "Browse"
+        searchVC.title = "Search"
+        libraryVC.title = "Library"
+        
+        homeVC.navigationItem.largeTitleDisplayMode = .always
+        searchVC.navigationItem.largeTitleDisplayMode = .always
+        libraryVC.navigationItem.largeTitleDisplayMode = .always
+        
+        let homeTab = UINavigationController(rootViewController: homeVC)
+        let searchTab = UINavigationController(rootViewController: searchVC)
+        let libraryTab = UINavigationController(rootViewController: libraryVC)
+        
+        homeTab.tabBarItem = UITabBarItem(title: "Home", image: UIImage(systemName: "house"), tag: 1)
+        searchTab.tabBarItem = UITabBarItem(title: "Search", image: UIImage(systemName: "magnifyingglass"), tag: 1)
+        libraryTab.tabBarItem = UITabBarItem(title: "Library", image: UIImage(systemName: "music.note.list"), tag: 1)
+        
+        homeTab.navigationBar.prefersLargeTitles = true
+        searchTab.navigationBar.prefersLargeTitles = true
+        libraryTab.navigationBar.prefersLargeTitles = true
+        
+        setViewControllers([homeTab, searchTab, libraryTab], animated: false)
+    }
 }
